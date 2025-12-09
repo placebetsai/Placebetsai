a198f804234bdf65c1e0dbab65476d4d3b606a21
placebetsai <placebetsai@gmail.com>
2025-12-09 10:27:16 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  // IMPORTANT:
-  // No `output: 'export'`
-  // No custom `distDir`
-  // No weird targets. Let Vercel and Next handle it.
+  reactStrictMode: true
 };
 
-export default nextConfig;;
+export default nextConfig;
